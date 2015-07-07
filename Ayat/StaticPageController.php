47773e5e77090ab7866a83946a39191883764f4a
Steven <info@artizen.be>
2015-07-08 00:26:24 +0200

    added map
    Ayat map

---
+++ Ayat/StaticPageController.php
@@ -0,0 +1,19 @@
+<?php namespace App\Http\Controllers;
+class StaticPageController extends Controller{
+    public function __construct(){
+        $this->middleware('guest');
+    }
+    public function contact(){
+        return view('staticPages.contact')->with(['voornaam' => 'Steven','achternaam' => 'Heynderickx']);
+    }
+   public function about(){
+    $name = 'Steven the Great';
+        return view('staticPages.about')->with ('name',$name);
+    }
+       public function people(){
+    // $names = ['Steven the Great','Steven','Ayat'];
+    $names = [];
+        return view('staticPages.people',compact('names'));
+        return view('staticPages.people',compact('names'));
+    }
+}
